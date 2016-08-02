079b2c51c4bdfb291206fc56104901f799214109
mscai <939546871@qq.com>
2016-08-02 10:55:11 +0800

    modified access_violation_handler.py and buffer_overflow.py

---
+++ buffer_overflow.py
@@ -0,0 +1,8 @@
+from ctypes import *
+
+msvcrt = cdll.msvcrt
+
+raw_input("Once the debugger is attached, press any key.")
+buffer = c_char_p("AAAAA")
+overflow = "A"*100
+msvcrt.strcpy(buffer,overflow)
